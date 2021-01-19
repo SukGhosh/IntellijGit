c39f2cf1286f98b1d42fef82f7fb86361d68818f
Sukanta Ghosh <Sukantaghosh.ee@gmail.com>
2021-01-18 21:32:43 -0500

    Direct Commit to main from Intellij Idea 18012021 21:32 Spacing issue in BarkingDog.java

--- src/main/java/sdetPreparation/BarkingDog.java
+++ src/main/java/sdetPreparation/BarkingDog.java
@@ -5,7 +5,7 @@ public class BarkingDog {
 	public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
 		if (hourOfDay < 0 || hourOfDay > 23) {
 			return false;
-		} else if (barking == false) {
+		} else if (!barking) {
 			return false;
 		} else if (hourOfDay < 8 || hourOfDay > 22) {
 			return true;
@@ -14,65 +14,4 @@ public class BarkingDog {
 		}
 
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
 }
